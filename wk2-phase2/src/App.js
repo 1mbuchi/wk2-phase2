092ad3fbc7d394c4f468318845a6376e50068631
1mbuchi Natasha <natashaimbuchi@gmail.com>
2023-07-31 14:06:12 +0300

    pushed

---
+++ wk2-phase2/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import BotCollection from './components/BotCollection';
+import YourBotArmy from './components/YourBotArmy';
+
+function App() {
+  const [botData, setBotData] = useState([]);
+  const [selectedBots, setSelectedBots] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/bots")
+      .then(response => response.json())
+      .then(data => setBotData(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  const enlistBot = (bot) => {
+    setSelectedBots(prevSelectedBots => {
+      if (!prevSelectedBots.includes(bot)) {
+        return [...prevSelectedBots, bot];
+      }
+      return prevSelectedBots;
+    });
+  };
+
+  const dischargeBot = (bot) => {
+    setSelectedBots(prevSelectedBots => prevSelectedBots.filter(selectedBot => selectedBot.id !== bot.id));
+  };
+
+  const deleteBot = (bot) => {
+    const updatedBotData = botData.filter(existingBot => existingBot.id !== bot.id);
+    setBotData(updatedBotData);
+    setSelectedBots(prevSelectedBots => prevSelectedBots.filter(selectedBot => selectedBot.id !== bot.id));
+  };
+
+  return (
+    <div className="App">
+      <h1>The Bot Plug</h1>
+      <YourBotArmy selectedBots={selectedBots} dischargeBot={dischargeBot} deleteBot={deleteBot} />
+      <BotCollection botData={botData} enlistBot={enlistBot} />
+    </div>
+  );
+}
+export default App;
